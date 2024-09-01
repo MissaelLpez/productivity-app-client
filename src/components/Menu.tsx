a0e5ff1df4a31bfe6fb1371abed228ba23f0025f
MissaelLpez <missaellpez@gmail.com>
2024-08-31 21:25:18 -0600

    add side menu

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,12 +1,26 @@
+import { setOpenMenu } from "@/store/slices/menuSlice";
 import { RootState } from "@/store/store";
 import { MenuIcon } from "lucide-react";
+import Drawer from "react-modern-drawer";
+import "react-modern-drawer/dist/index.css";
 import { useDispatch, useSelector } from "react-redux";
 
 const Menu = () => {
-  const isOpen = useSelector((state: RootState) => state.menu.open);
+  const isOpen = useSelector((state: RootState) => state.menu.menuOpen);
   const dispatch = useDispatch();
 
-  return <MenuIcon />;
+  return (
+    <div className="block lg:hidden">
+      <MenuIcon onClick={() => dispatch(setOpenMenu())} />
+      <Drawer
+        open={isOpen}
+        onClose={() => dispatch(setOpenMenu())}
+        direction="left"
+      >
+        <div className="bg-white dark:bg-dark h-screen">Options</div>
+      </Drawer>
+    </div>
+  );
 };
 
 export default Menu;
--- src/store/slices/menuSlice.tsx
+++ src/store/slices/menuSlice.tsx
@@ -1,23 +1,23 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 export interface MenuState {
-  open: boolean;
+  menuOpen: boolean;
 }
 
 const initialState: MenuState = {
-  open: false,
+  menuOpen: false,
 };
 
 export const menuSlice = createSlice({
   name: "menu",
   initialState,
   reducers: {
-    setOpen: (state) => {
-      state.open = !open;
+    setOpenMenu: (state) => {
+      state.menuOpen = !state.menuOpen;
     },
   },
 });
 
-export const { setOpen } = menuSlice.actions;
+export const { setOpenMenu } = menuSlice.actions;
 
 export default menuSlice.reducer;
