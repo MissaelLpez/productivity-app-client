d083d98c99588a8905b9233802e27a39d1b6200e
MissaelLpez <missaellpez@gmail.com>
2024-09-05 21:35:42 -0600

    custom minutes

--- src/components/tasks/CreateTask.tsx
+++ src/components/tasks/CreateTask.tsx
@@ -23,12 +23,24 @@ const CreateTask = () => {
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
-    const form = Object.fromEntries(
-      new FormData(e.target as HTMLFormElement)
-    ) as unknown as CreateTaskInput;
+    const form = Object.fromEntries(new FormData(e.target as HTMLFormElement));
+
+    const { name, description, defined_time, custom_time } = form;
+
+    const milliseconds =
+      custom_time !== ""
+        ? String(Number(custom_time) * 60 * 1000)
+        : String(defined_time);
+
+    const data: CreateTaskInput = {
+      name: String(name),
+      description: String(description),
+      defined_time: milliseconds,
+      redefined_time: milliseconds,
+    };
 
     createTask({
-      createTaskInput: { ...form, redefined_time: form.defined_time },
+      createTaskInput: data,
     });
   };
 
@@ -64,19 +76,43 @@ const CreateTask = () => {
             />
           </label>
 
-          <label>
-            Tiempo
-            <Select name="defined_time">
-              <SelectTrigger className="w-1/3 mt-2 p-2 outline-none rounded-xl border border-primary-500">
-                <SelectValue placeholder="Definir tiempo" />
-              </SelectTrigger>
-              <SelectContent className="text-white outline-none">
-                <SelectItem value="1800000">15 mins.</SelectItem>
-                <SelectItem value="3600000">30 mins.</SelectItem>
-                <SelectItem value="7200000">1 hora</SelectItem>
-              </SelectContent>
-            </Select>
-          </label>
+          <div className="flex w-full gap-x-10">
+            <label className="w-1/2">
+              Tiempo
+              <Select name="defined_time" defaultValue="3600000">
+                <SelectTrigger className="w-full mt-2 p-2 outline-none rounded-xl border border-primary-500">
+                  <SelectValue placeholder="Definir tiempo" />
+                </SelectTrigger>
+                <SelectContent className="text-white outline-none bg-white dark:bg-dark">
+                  <SelectItem value="3600000">30 mins.</SelectItem>
+                  <SelectItem value="5400000">45 mins.</SelectItem>
+                  <SelectItem value="7200000">1 hora</SelectItem>
+                </SelectContent>
+              </Select>
+            </label>
+
+            <label className="w-1/2">
+              Definir minutos
+              <input
+                type="number"
+                name="custom_time"
+                placeholder="90"
+                className="bg-transparent border border-primary-500 rounded-xl p-2 w-full mt-2 mb-4 outline-none"
+                autoComplete="off"
+                title="Escribe un tiempo en minutos"
+                min={1}
+                max={120}
+                onChange={(e) => {
+                  if (Number(e.target.value) > 120) {
+                    e.target.value = "120";
+                  }
+                  if (Number(e.target.value) < 0) {
+                    e.target.value = "0";
+                  }
+                }}
+              />
+            </label>
+          </div>
 
           <Button type="submit" className="my-5 mx-auto rounded-xl w-1/3">
             Guardar
--- src/components/tasks/TaskCountdown.tsx
+++ src/components/tasks/TaskCountdown.tsx
@@ -1,5 +1,9 @@
+import useUpdateTask from "@/api/mutations/useUpdateTask";
 import useCountdown from "@/hooks/useCountdown";
 import useGetTaskById from "@/hooks/useGetTaskById";
+import { setOpenTask } from "@/store/slices/modalSlice";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
 
 interface Props {
   taskId: number;
@@ -8,10 +12,26 @@ interface Props {
 
 const TaskCountdown = ({ size = "text-3xl", taskId }: Props) => {
   const { task } = useGetTaskById(taskId);
+  const { mutate: updateTask } = useUpdateTask();
+  const dispatch = useDispatch();
 
+  const currentTime = Date.now();
   const targetTime = new Date(String(task?.finish_in));
 
   const [days, hours, minutes, seconds] = useCountdown(Number(targetTime));
+
+  useEffect(() => {
+    if (Number(hours) <= 0 && Number(minutes) <= 0 && Number(seconds) <= 0) {
+      updateTask({
+        updateTaskInput: {
+          id: Number(task?.id),
+          status: "completed",
+          completed_at: currentTime,
+        },
+      });
+      dispatch(setOpenTask(null));
+    }
+  }, [currentTime, dispatch, hours, minutes, seconds, task?.id, updateTask]);
 
   const formatTime = (time: number | undefined) =>
     Number(time).toString().padStart(2, "0");
